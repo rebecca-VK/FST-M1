60bf891df56ba1c0ae9f08dc193c72edda083d42
rebecca-VK <rebecca.v@ibm.com>
2024-12-30 15:35:53 +0530

    Update Activity1.java

--- JUnit/Activities/Activity1.java
+++ JUnit/Activities/Activity1.java
@@ -1,9 +1,15 @@
 package activities;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class Activity1 {
     // Test fixtures
@@ -20,29 +26,39 @@ public class Activity1 {
     // Test method to test the insert operation
     @Test
     public void insertTest() {
+    	
+    	List<String> expectedList = Arrays.asList("alpha","beta","gamma");
         // Assertion for size
         assertEquals(2, list.size(), "Wrong size");
         // Add new element
-        list.add(2, "charlie");
+        list.add(2, "gamma");
         // Assert with new elements
         assertEquals(3, list.size(), "Wrong size");
 
-        // Assert individual elements
-        assertEquals("alpha", list.get(0), "Wrong element");
-        assertEquals("beta", list.get(1), "Wrong element");
-        assertEquals("charlie", list.get(2), "Wrong element");
+		/*
+		 * // Assert individual elements assertAll( () -> assertEquals("alpha",
+		 * list.get(0), "Wrong element"), () -> assertEquals("beta", list.get(1),
+		 * "Wrong element"), () -> assertEquals("charlie", list.get(2), "Wrong element")
+		 * );
+		 */
+        assertIterableEquals(expectedList, list);
     }
 
     // Test method to test the replace operation
     @Test
     public void replaceTest() {
-        // Replace new element
-        list.set(1, "charlie");
+        // Expected List
+    	List<String> expectedList = Arrays.asList("alpha","gamma");
+    	//Replaced Value
+        list.set(1, "gamma");
 
         // Assert size of list
         assertEquals(2, list.size(), "Wrong size");
         // Assert individual elements
-        assertEquals("alpha", list.get(0), "Wrong element");
-        assertEquals("charlie", list.get(1), "Wrong element");
+		/*
+		 * assertAll( () -> assertEquals("alpha", list.get(0), "Wrong element"), () ->
+		 * assertEquals("charlie", list.get(1), "Wrong element") );
+		 */
+        assertIterableEquals(expectedList, list);
     }
 }
